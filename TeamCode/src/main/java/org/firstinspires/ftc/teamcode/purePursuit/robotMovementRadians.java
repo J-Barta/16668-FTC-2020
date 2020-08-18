fbd99d2fa69e559509ff92fc40257191d589f0e7
J-Barta <53406552+J-Barta@users.noreply.github.com>
2020-08-18 09:45:50 -0400

    Acer Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/purePursuit/robotMovementRadians.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/purePursuit/robotMovementRadians.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.Range;
 
+//Test
 @Autonomous(name="Robot Movement Radians")
 public class robotMovementRadians extends LinearOpMode {
     globalCoordinatePosition globalPositionUpdate;
